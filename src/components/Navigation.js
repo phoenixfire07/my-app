102661bc0be593eaaa5bcca5c03e27b6670a8b2c
phoenixfire07 <32801493+phoenixfire07@users.noreply.github.com>
2019-08-20 19:13:28 +0100

    Toggle drawer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,26 +10,23 @@ export default function Navigation() {
     left: false
   });
 
-  const toggleDrawer = (side, open) => event => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
+  function toggleDrawer(side, open){
     setState({ ...state, [side]: open });
   };
 
   return (
     <Fragment>
       <div className="pt-5"></div>
-      <div onClick={toggleDrawer('left', true)} className="navigation-bar d-flex align-items-center justify-content-center">
+      <div onClick={() => toggleDrawer('left', true)} className="navigation-bar d-flex align-items-center justify-content-center">
         <MenuRounded className="burger-menu"></MenuRounded>
       </div>
-      <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
-      <Close className="burger-menu ml-auto pr-1 pt-1" onClick={toggleDrawer('left', false)}></Close>
+      <Drawer open={state.left} onClose={() => toggleDrawer('left', false)}>
+      <Close className="burger-menu ml-auto pr-1 pt-1" onClick={() => toggleDrawer('left', false)}></Close>
         <div className="d-flex h-100 align-items-center justify-content-center flex-column p-5">
-          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("about-section").scrollIntoView()}}>About</h3>
-          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("experience-section").scrollIntoView()}}>Experience</h3>
-          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("skillset-section").scrollIntoView()}}>Skillset</h3>
-          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("contact-section").scrollIntoView()}}>Contact</h3>
+          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("about-section").scrollIntoView(); }}>About</h3>
+          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("experience-section").scrollIntoView();}}>Experience</h3>
+          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("skillset-section").scrollIntoView();}}>Skillset</h3>
+          <h3 className="pt-4 menu-option" onClick={() => { toggleDrawer('left', false); document.getElementById("contact-section").scrollIntoView();}}>Contact</h3>
        </div>
       </Drawer>
     </Fragment>
