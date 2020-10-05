68410fc649fa238f3dba375428afe99478ca88e1
Phoebe Staab <phoebestaab@gmail.com>
2020-10-05 15:44:45 -0700

    stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
       <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>       
         <div id="contact-section" className=" pt-4 contact-container">
             <h1 className="contact-heading pl-2 pb-3">Get in touch</h1>
-            <a href="tel:+1-203-550-8303" className="contact-text pl-3 pt-3">(US) 203-550-8303</a>
+            <a href="tel:+1-203-550-8303" className="contact-text pl-3 pt-3">203-550-8303</a>
             <br></br>
             <a href = "mailto: phoebestaab@gmail.com" className="contact-text pl-3 pt-3">phoebestaab@gmail.com</a>
             <br></br>
