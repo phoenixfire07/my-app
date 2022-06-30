edcf45044b86e94c17d7f519764efeb4e0297be5
Phoebe Staab <phoebestaab@gmail.com>
2022-06-30 08:43:26 -0400

    add phd

--- src/data/experience.js
+++ src/data/experience.js
@@ -2,11 +2,11 @@ export const experienceData = [
   {
     "type":"school",
     "about":null,
-    "years":"Jan 2022 - Jan 2022 (Currently enrolled)",
+    "years":"Jun 2022 - Jun 2025 (Expected)",
     "location_city":"Kings College London | London, UK",
     "role_name": null,
-    "responsibilities":"The degree covers a comprehensive and integrated exploration of our current understanding of the psychological and neuroscientific basis of mental health.",
-    "name":"MSc Psychology & Neuroscience of Mental Health",
+    "responsibilities":"The primary focus of my research is developing digital interventions which focus on developing metacognitive awareness of interoception.",
+    "name":"PhD Human Computer Interaction",
 
   },
   {
