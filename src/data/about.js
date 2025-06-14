cb3ead42bafbc7356162dda947645c6e80aeede3
phoebe-staab <32801493+phoenixfire07@users.noreply.github.com>
2025-06-14 13:05:43 +0200

    Update about.js

--- src/data/about.js
+++ src/data/about.js
@@ -1,11 +1,11 @@
 export const aboutData = [
   {
     "title":"Hi, I'm Phoebe",
-    "blurb":" I am a product-oriented software engineer and tech lead with a PhD in Human-Computer Interaction (pending defense) pivoting to product management. My PhD focused on designing and developing a digital intervention which can support the development of healthy interoception (mindful awareness of the body)."
+    "blurb":"I am a product-oriented software engineer and tech lead with a PhD in Human-Computer Interaction (pending defense), currently pivoting to product management. With experience building and shipping mental health and ed-tech products, I am passionate about creating technology that improves lives and contributes to a better world."
   },
   {
     "title":"Professional Interests",
-    "blurb":"Broadly, I am interested in developing technology which promotes wellbeing and human flourishing. And while this is easy to claim, it is challenging to practice. I believe that as technologists (and people), we are responsible for informing ourselves such that we can understand, respect and respond to the unique needs of individuals and develop technology only if it can truly support those needs."
+    "blurb":"Broadly, I am interested in building technology-for-good. I believe that as technologists (and people), we are responsible for informing ourselves so we can understand, respect, and respond to the unique needs of individuals, and develop technology only when it can truly support those needs."
   },
   {
     "title":"Personal Interests",
