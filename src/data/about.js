abd1840a58d4ffa9a4226fbd9a82232f40ff0aa8
phoebe-staab <32801493+phoenixfire07@users.noreply.github.com>
2025-06-14 09:36:37 +0200

    Update about.js

--- src/data/about.js
+++ src/data/about.js
@@ -1,11 +1,11 @@
 export const aboutData = [
   {
     "title":"Hi, I'm Phoebe",
-    "blurb":"I am a software engineer developing education technology at Pluralsight and PhD candidate at King's College London. My research is focused on designing and developing digital interventions which can support the development of healthy interoception (awareness of body)."
+    "blurb":" I am a product-oriented software engineer and tech lead with a PhD in Human-Computer Interaction (pending defense) pivoting to product management. My PhD focused on designing and developing a digital intervention which can support the development of healthy interoception (mindful awareness of the body)."
   },
   {
     "title":"Professional Interests",
-    "blurb":"Broadly, I am interested in developing technology which promotes wellbeing. And while this is easy to claim, it is challenging to practice. I believe that as technologists (and people), we are responsible for informing ourselves such that we can understand, respect and respond to the unique needs of individuals and develop technology only if it can truly support those needs."
+    "blurb":"Broadly, I am interested in developing technology which promotes wellbeing and human flourishing. And while this is easy to claim, it is challenging to practice. I believe that as technologists (and people), we are responsible for informing ourselves such that we can understand, respect and respond to the unique needs of individuals and develop technology only if it can truly support those needs."
   },
   {
     "title":"Personal Interests",
