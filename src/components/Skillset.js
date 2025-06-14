881a9916c6db3661b4b719da7c712993778878a0
phoebe-staab <32801493+phoenixfire07@users.noreply.github.com>
2025-06-14 11:44:49 +0200

    Update Skillset.js

--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -1,8 +1,4 @@
 import React, {Fragment} from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
-import {frontendSkillsData, backendSkillsData, otherSkillsData, dataSkillsData} from '../data/skillset';
-
-
 
 export default function Skillset() {
 
