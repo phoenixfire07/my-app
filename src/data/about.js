b1d3967d1a5bea16093365947870c28f359ac85f
phoebe-staab <32801493+phoenixfire07@users.noreply.github.com>
2026-02-22 09:35:35 -0800

    Update about.js

--- src/data/about.js
+++ src/data/about.js
@@ -1,14 +1,13 @@
 export const aboutData = [
   {
     "title":"Hi, I'm Phoebe",
-    "blurb":"I am a product-oriented software engineer and tech lead who just finished a PhD in Human-Computer Interaction (pending defense). I am currently pivoting to a more product oriented role where I can make use of my research, UX, analysis, and technical skillset. With experience building and shipping mental health and ed-tech products, I am passionate about creating technology that contributes to a better world."
-  },
+    "blurb": "I am a Product Manager with a background as an engineer and tech lead and a PhD in Human–Centered Computing. I lead patient experience team at Solace, drawing on my research, UX, analytical, and technical expertise to build and scale healthcare technology. With experience shipping healthcare, mental health, and education products, I am motivated by creating technology that meaningfully improves people’s lives."  },
   {
     "title":"Professional Interests",
     "blurb":"Broadly, I am interested in building technology-for-good. I believe that as technologists (and people), we are responsible for informing ourselves so we can understand, respect, and respond to the unique needs of individuals, and develop technology only when it can truly support those needs."
   },
   {
     "title":"Personal Interests",
-    "blurb":"I am a fermentation nerd and I actually do really like long walks (or runs) on the beach. I love hiking and foraging for mushrooms. I am into yin yoga, meditation, reading, making ceramics and quiet time. I am a huge fan of Startrek and I love a good cat video."
+    "blurb":"I am a ceramics nerd and I actually do really like long walks (or runs) on the beach. I love hiking, fermentation, and foraging for mushrooms. I am into yin yoga, meditation, reading, making ceramics and quiet time. I am a huge fan of Startrek and I love a good cat video."
   },
 ];
